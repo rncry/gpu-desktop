2f6b924b94280605ce23a89bc676303187d0f91c
Aaron <user@example.com>
2015-03-16 11:05:15 +0000

    Fixes for VGL and xinit

--- setup-desktop.sh
+++ setup-desktop.sh
@@ -8,11 +8,11 @@ sudo yum install -y wget
 wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 sudo rpm -ivh epel-release-7-5.noarch.rpm
 sudo yum groups install -y "MATE Desktop"
-wget http://virtualgl.sourceforge.net/vgl.nightly/VirtualGL-2.3.91.x86_64.rpm
+wget https://github.com/rncry/gpu-desktop/raw/master/VirtualGL-2.4.1.x86_64.rpm
 wget https://github.com/rncry/gpu-desktop/raw/master/turbovnc-1.2.80.x86_64.rpm
 sudo yum install -y libXaw libXmu libXt xauth xdpyinfo glx-utils libXp xterm xorg-x11-xdm  xorg-x11-fonts-100dpi xorg-x11-fonts-ISO8859-9-100dpi xorg-x11-fonts-misc xorg-x11-fonts-Type1 gcc kernel-devel libGLU
 sudo rpm -ivh turbovnc-1.2.80.x86_64.rpm
-sudo rpm -ivh VirtualGL-2.3.91.x86_64.rpm
+sudo rpm -ivh VirtualGL-2.4.1.x86_64.rpm
 wget http://us.download.nvidia.com/XFree86/Linux-x86_64/346.47/NVIDIA-Linux-x86_64-346.47.run
 chmod +x NVIDIA-Linux-x86_64-346.47.run
 sudo ./NVIDIA-Linux-x86_64-346.47.run -s -Z
@@ -37,12 +37,7 @@ sudo reboot
 while ! ssh -i "$1" -l "$2" -t "$3" bash -c "echo 'hi world'" &>/dev/null; do :; done
 
 ssh -i "$1" -l "$2" -t "$3" bash -c "'
-sudo xinit
-#export PASS=` < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8};echo;`
-#echo "$PASS" | /opt/TurboVNC/bin/vncpasswd -f
-#touch $HOME/.vnc/passwd
-#echo "$PASS" | /opt/TurboVNC/bin/vncpasswd -f >> $HOME/.vnc/passwd
-#chmod 600 ~/.vnc/passwd
+sudo xinit &
 sudo iptables -I INPUT -p tcp --dport 5901 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 5801 -j ACCEPT
 sudo iptables-save
